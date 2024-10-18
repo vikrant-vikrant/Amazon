e54d15435c4758efe1d7e8748f5fe5e57aaff83b
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-10-18 12:17:50 +0530

    test some function using jasmin

---
+++ tests-jasmin/moneyTest.js
@@ -0,0 +1,13 @@
+import { formatCurrency } from "../public/js/money.js";
+
+describe('test suite: formateCurrency',()=>{
+  it('converts cents into dollars',()=>{
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+  it('works with zero',()=>{
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+  it('round up to the nearest cent',()=>{
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+});

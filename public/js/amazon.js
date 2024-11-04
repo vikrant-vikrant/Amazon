37575c4b3c4d498c003f95bbc09c3f4e964fea22
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-11-04 21:38:17 +0530

    use inheritance and polymorphism to add size chart

--- public/js/amazon.js
+++ public/js/amazon.js
@@ -38,6 +38,7 @@ products.forEach((product) => {
               <option value="10">10</option>
             </select>
           </div>
+          ${product.extraInfoHTML()}
           <div class="product-spacer"></div>
           <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
--- public/js/products.js
+++ public/js/products.js
@@ -27,6 +27,24 @@ class Product{
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`;
   }
+  extraInfoHTML(){
+    return '';
+  }
+}
+class Clothing extends Product{
+  sizeChartLink;
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+  extraInfoHTML(){
+    //super.extraInfoHTML();
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">
+      Size chart
+    </a>
+    `;
+  }
 }
 export const products = [
   {
@@ -846,5 +864,8 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+  if(productDetails.type==='clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });

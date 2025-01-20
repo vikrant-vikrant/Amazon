41c3d1856cc8dd12c01e5493751996ceb9947f8c
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2025-01-20 21:01:43 +0530

    Create order using the backend

--- public/js/checkout/paymentSummery.js
+++ public/js/checkout/paymentSummery.js
@@ -2,7 +2,7 @@ import { cart } from "/js/cart-class.js";
 import { getProduct } from "/js/products.js";
 import { getDeliveryOption } from "/js/deliveryOptions.js";
 import { formatCurrency } from "/js/money.js";
-
+import { addOrder } from "../order.js";
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
@@ -16,3 +16,25 @@ export function renderPaymentSummary() {
   const totalPayment = formatCurrency(productPriceCents + shippingPriceCents);
   return { productPriceCents, shippingPrice, totalPayment };
 }
+document
+  .querySelector(".js-place-order")
+  .addEventListener("click", async () => {
+    try {
+      const response = await fetch("https://supersimplebackend.dev/orders", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          cart: cart,
+        }),
+      });
+
+      const order = await response.json();
+      addOrder(order);
+    } catch (error) {
+      console.log(`Unexpected error, Try again later`,error);
+      alert(error);
+    }
+    // window.location.href = 'orders.html';
+  });
---
+++ public/js/order.js
@@ -0,0 +1,10 @@
+
+export const order = JSON.parse(localStorage.getItem('orders')) || [];
+export function addOrder(order){
+  // order.unshift(order);
+  saveToStroage();
+}
+
+function saveToStroage(){
+  localStorage.setItem('orders', JSON.stringify(order));
+}

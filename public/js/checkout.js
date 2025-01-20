18213a0611b8c949fa88ba0f374633341ceef784
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2025-01-20 12:28:25 +0530

    Refactor checkout.js to use async/await for product loading and order summary rendering

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,36 +1,31 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { loadProducts } from "./products.js";
-//callbacks
-// loadProducts(() => {
-//   renderOrderSummary();
-// });
-
-new Promise((resolve, reject) => {
-  try {
-    loadProducts(() => {
-      resolve();
-    });
-  } catch (error) {
-    reject("Failed to load products.");
-  }
-})
-  .then(() => {
-    renderOrderSummary();
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-// initializeCart();
-// async function initializeCart() {
+// new Promise((resolve, reject) => {
 //   try {
-//     await new Promise((resolve, reject) => {
-//       loadProducts(() => {
-//         resolve();
-//       });
+//     loadProducts(() => {
+//       resolve();
 //     });
-//     renderOrderSummary();
 //   } catch (error) {
-//     alert(error);
+//     reject("Failed to load products.");
 //   }
-// }
+// })
+//   .then(() => {
+//     renderOrderSummary();
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+  async function initializeCart() {
+      try {
+    await new Promise((resolve, reject) => {
+      loadProducts(() => {
+        resolve();
+      });
+    });
+    renderOrderSummary();
+  } catch (error) {
+    alert(error);
+  }
+}
+initializeCart();

e6f7d4f78ff3f9fb8d899282b0873958d5d6d39c
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2025-02-03 10:18:34 +0530

    add jasmin

---
+++ moneyTest/moneyTest.js
@@ -0,0 +1,22 @@
+// import { formatCurrency } from "../../public/js/money.js";
+// describe("Test suite : formatCurrency", () => {
+//   it("Converts cents into dollars", () => {
+//     expect(formatCurrency(2000)).toEqual("20.00");
+//   });
+//   it("Works with zero", () => {
+//     expect(formatCurrency(0)).toEqual("0.00");
+//   });
+//   it("round up to the nearest cent", () => {
+//     expect(formatCurrency(2000.5)).toEqual("20.01");
+//   });
+// });
+
+describe("A suite is just a function", function () {
+  let a;
+
+  it("and so is a spec", function () {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+});
